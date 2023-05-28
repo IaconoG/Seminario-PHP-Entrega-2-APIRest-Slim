1b55cb2d0e75909505b9228fd3be8f91ba276221
iacono gianfranco <iacono.gian13@gmail.com>
2023-05-28 01:29:48 -0300

    clean home controller

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,46 +1,7 @@
 <?php
 namespace App\Controllers; // -> esta linea determina la carpeta donde se encuentra el archivo
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-use App\Models\DB;
-
-$app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
-$app->setBasePath("/src/controllers");
-
-// Home
-$app->get('/juegos/all', function(Request $req, Response $res, $args) {
-  $sql = "SELECT * FROM juegos";
-
-  try {
-      $db = new DB();
-      $db = $db->getConnection();
-      $resultado = $db->query($sql);
-
-  if ($resultado->rowCount() > 0) {
-      $juegos = $resultado->fetchAll(PDO::FETCH_OBJ);
-      $res->getBody()->write(json_encode($juegos));
-  } else {
-      $res->getBody()->write(json_encode("No existen juegos en la base de datos"));
-  }
-  $resultado = null;
-  $db = null;
-
-  return $res->withHeader('Content-Type', 'application/json')->withStatus(200);
-  } catch (PDOException $e) {
-      echo '{"error" : {"text": '.$e->getMessage().' }';
-  };
-});
-
-// Obtener todos los juegos
-// $app->get('/juegos/', function (Request $req, Response $res, array $args) {
-//   $db = new DB();
-
-//   $res = 
-
-// });
+// HOMECONTROLLER -> Sireve para manejar las peticiones de la pagina principal
 
 
 

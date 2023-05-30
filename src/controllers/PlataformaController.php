c259645599fa523fb07c4202bc9c7015463493ff
iacono gianfranco <iacono.gian13@gmail.com>
2023-05-30 00:40:56 -0300

    fix: nombre de metodos del objeto plataforma

--- src/controllers/PlataformaController.php
+++ src/controllers/PlataformaController.php
@@ -10,23 +10,23 @@ use App\Views\JuegoView;
 
 class PlataformaController extends Controller{
   // --- METODO PARA MOSTRAR TODOS LAS PLATAFORMAS ---
-  public function listarJuegos(Request $req, Response $res, $args) {
+  public function listarPlataformas(Request $req, Response $res, $args) {
     return $this->obtenerTodos(new Plataforma(), 'plataformas', $res);
   }
   // --- METODO PARA OBTENER UNA UNICA PLATAFORMA ---
-  public function obtenerJuego(Request $req, Response $res, $args) {
+  public function obtenerPlataforma(Request $req, Response $res, $args) {
     return $this->obtenerUnico(new Plataforma(), 'plataformas', $args['id'], $res);
   }
   // --- METODO PARA CREAR UNA PLATAFORMA ---
-  public function crearJuego(Request $req, Response $res, $args) { 
+  public function crearPlataforma(Request $req, Response $res, $args) { 
     return $this->crear(new Plataforma(), 'plataformas', $req->getParsedBody(), $req->getUploadedFiles(), $res);
   }
   // --- METODO PARA ELIMINAR UNA PLATAFORMA ---
-  public function eliminarJuego(Request $req, Response $res, $args) {
+  public function eliminarPlataforma(Request $req, Response $res, $args) {
     return $this->elimnar(new Plataforma(), 'plataformas', $args['id'], $res);
   }
   // --- METODO PARA ACTUALIZAR UNA PLATAFORMA ---
-  public function actualizarJuego(Request $req, Response $res, $args) {
+  public function actualizarPlataforma(Request $req, Response $res, $args) {
     return $this->actualizar(new Plataforma(), 'plataformas', $args['id'], $req->getParsedBody(), $req->getUploadedFiles(), $res);
   }
 }

7c9bdefc92dc66763573b33ac244f4f17460927d
iacono gianfranco <iacono.gian13@gmail.com>
2023-05-30 00:37:21 -0300

    Correcion en los metodos patch

--- public/index.php
+++ public/index.php
@@ -18,37 +18,37 @@ $app->get('/', function (Request $req, Response $res, array $args) {
 
 use App\Controllers\GeneroController;
 // Ruta para mostrar todos los generos
-$app->get('/generos/todos', GeneroController::class . ':listarGeneros');
+$app->get('/generos/obtener/todos', GeneroController::class . ':listarGeneros');
 // Ruta para crear un nuevo genero
 $app->post('/generos/crear', GeneroController::class . ':crearGenero');
 // Ruta para eliminar un genero
 $app->delete('/generos/eliminar/{id}', GeneroController::class . ':eliminarGenero');
 // Ruta para actualizar un genero
-$app->patch('/generos/actualizar/{id}', GeneroController::class . ':actualizarGenero'); 
+$app->post('/generos/actualizar/{id}', GeneroController::class . ':actualizarGenero'); // POST porque PATCH no quiere evniar los datos | El formulario tiene un campo que cambio el metodo enviado a patch
 // Ruta para obtener un genero
 $app->get('/generos/obtener/{id}', GeneroController::class . ':obtenerGenero');
 
 use App\Controllers\JuegoController;
 // Ruta para mostrar todos los juegos
-$app->get('/juegos/todos', JuegoController::class . ':listarJuegos');
+$app->get('/juegos/obtener/todos', JuegoController::class . ':listarJuegos');
 // Ruta para crear un nuevo genero
 $app->post('/juegos/crear', JuegoController::class . ':crearJuego');
 // Ruta para eliminar un genero
 $app->delete('/juegos/eliminar/{id}', JuegoController::class . ':eliminarJuego');
 // Ruta para actualizar un genero
-$app->post('/juegos/actualizar/{id}', JuegoController::class . ':actualizarJuego'); // POST porque PATCH no quiere evniar los files | El formulario tiene un campo que cambio el metodo enviado a patch
+$app->post('/juegos/actualizar/{id}', JuegoController::class . ':actualizarJuego'); // POST porque PATCH no quiere evniar los datos | El formulario tiene un campo que cambio el metodo enviado a patch
 // Ruta para obtener un genero
 $app->get('/juegos/obtener/{id}', JuegoController::class . ':obtenerJuego');
 
 use App\Controllers\PlataformaController;
 // Ruta para mostrar todos los juegos
-$app->get('/plataformas/todos', PlataformaController::class . ':listarPlataformas');
+$app->get('/plataformas/obtener/todos', PlataformaController::class . ':listarPlataformas');
 // Ruta para crear un nuevo genero
 $app->post('/plataformas/crear', PlataformaController::class . ':crearPlataforma');
 // Ruta para eliminar un genero
 $app->delete('/plataformas/eliminar/{id}', PlataformaController::class . ':eliminarPlataforma');
 // Ruta para actualizar un genero
-$app->patch('/plataformas/actualizar/{id}', PlataformaController::class . ':actualizarPlataforma'); 
+$app->post('/plataformas/actualizar/{id}', PlataformaController::class . ':actualizarPlataforma'); // POST porque PATCH no quiere evniar los datos | El formulario tiene un campo que cambio el metodo enviado a patch
 // Ruta para obtener un genero
 $app->get('/plataformas/obtener/{id}', PlataformaController::class . ':obtenerPlataforma');
 

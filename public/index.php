eed04cf7666550d1e8994a17e61796bed3a59595
iacono gianfranco <iacono.gian13@gmail.com>
2023-06-06 16:34:46 -0300

    cambios en la url de los endpoints

--- public/index.php
+++ public/index.php
@@ -18,35 +18,35 @@ $app->get('/', function (Request $req, Response $res, array $args) {
 
 use App\Controllers\GeneroController;
 // Ruta para mostrar todos los generos
-$app->get('/generos/buscar/todos', GeneroController::class . ':listarGeneros');
+$app->get('/generos/todos', GeneroController::class . ':listarGeneros');
 // Ruta para crear un nuevo genero
-$app->post('/generos/crear', GeneroController::class . ':crearGenero');
+$app->post('/generos', GeneroController::class . ':crearGenero');
 // Ruta para eliminar un genero
-$app->delete('/generos/eliminar/{id}', GeneroController::class . ':eliminarGenero');
+$app->delete('/generos/{id}', GeneroController::class . ':eliminarGenero');
 // Ruta para actualizar un genero
-$app->patch('/generos/actualizar/{id}', GeneroController::class . ':actualizarGenero'); 
+$app->patch('/generos/{id}', GeneroController::class . ':actualizarGenero'); 
 
 use App\Controllers\JuegoController;
 // Ruta para mostrar todos los juegos
-$app->get('/juegos/buscar/todos', JuegoController::class . ':listarJuegos');
+$app->get('/juegos', JuegoController::class . ':listarJuegos');
 // Ruta para crear un nuevo juegos
-$app->post('/juegos/crear', JuegoController::class . ':crearJuego');
+$app->post('/juegos', JuegoController::class . ':crearJuego');
 // Ruta para eliminar un juego
-$app->delete('/juegos/eliminar/{id}', JuegoController::class . ':eliminarJuego');
+$app->delete('/juegos/{id}', JuegoController::class . ':eliminarJuego');
 // Ruta para actualizar un juegos
-$app->patch('/juegos/actualizar/{id}', JuegoController::class . ':actualizarJuego'); 
+$app->patch('/juegos/{id}', JuegoController::class . ':actualizarJuego'); 
 // Ruta para buscar un juegos
 $app->get('/juegos/buscar', JuegoController::class . ':buscarJuegos'); // juegos/buscar?nombre=&id_genero=&id_plataforma=&orden=
 
 use App\Controllers\PlataformaController;
 // Ruta para mostrar todos las plataformas
-$app->get('/plataformas/buscar/todos', PlataformaController::class . ':listarPlataformas');
+$app->get('/plataformas', PlataformaController::class . ':listarPlataformas');
 // Ruta para crear una nueva plataforma
-$app->post('/plataformas/crear', PlataformaController::class . ':crearPlataforma');
+$app->post('/plataformas', PlataformaController::class . ':crearPlataforma');
 // Ruta para eliminar una plataforma
-$app->delete('/plataformas/eliminar/{id}', PlataformaController::class . ':eliminarPlataforma');
+$app->delete('/plataformas/{id}', PlataformaController::class . ':eliminarPlataforma');
 // Ruta para actualizar una plataforma
-$app->patch('/plataformas/actualizar/{id}', PlataformaController::class . ':actualizarPlataforma'); 
+$app->patch('/plataformas/{id}', PlataformaController::class . ':actualizarPlataforma'); 
 
 
 

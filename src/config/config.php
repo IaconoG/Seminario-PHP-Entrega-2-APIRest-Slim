4861b7ba3994eead6cb53c7cefa4d8e15a128de6
iacono gianfranco <iacono.gian13@gmail.com>
2023-06-19 04:09:08 -0300

    Ajuste de configuracion de errores

---
+++ src/config/config.php
@@ -0,0 +1,11 @@
+<?php
+// Archivo de configuración global
+
+// Configuración para ocultar las advertencias "Notice" y los "Warning"
+error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
+  // error_reporting() -> establece qué niveles de error se muestran
+  // E_ALL -> todos los errores excepto E_STRICT
+  // ~E_WARNING -> todos los errores excepto E_WARNING
+  // ~E_NOTICE -> todos los errores excepto E_NOTICE
+  // Esto para evitar que se modifiuqe el mensjae devuleto de error
+  
